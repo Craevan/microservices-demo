747c05ebce102c62e493eba13b75fa53f8424eaf
Crevan <a.siverskii@gmail.com>
2023-12-19 12:30:30 +0300

    simple example for Microservices with Spring Cloud

---
+++ api-gateway/src/test/java/dev/crevan/demo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.crevan.demo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
